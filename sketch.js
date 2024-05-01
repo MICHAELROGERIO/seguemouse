28eca9c90c4e133dda53c66bec9be79fa3cd6fbb
MICHAELROGERIO <168595807+MICHAELROGERIO@users.noreply.github.com>
2024-05-01 15:04:28 -0300

    adicionando arquivos js, css

---
+++ sketch.js
@@ -0,0 +1,11 @@
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
+function setup() {
+  createCanvas(400, 400);
+   background(color(2200,80,1800));
+  cor = color(random(0,220), random(0,220), random(0,220));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
